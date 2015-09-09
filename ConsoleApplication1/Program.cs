6deccdb65efe2882d34e3095687dc04c919e2e82
alainosorio <alainosorio@gmail.com>
2015-09-09 01:36:29 -0500

    RobotName

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Robot jhonn = new Robot();
+
+            Robot anna = new Robot();
+
+            if (anna.Name() == jhonn.Name())
+            {
+                anna = new Robot();
+            }
+
+            Console.WriteLine(jhonn.Name());
+
+            Console.WriteLine(anna.Name());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/Robot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    public class Robot
+    {
+        public string Name()
+        {
+            //Utilizo la funcionalidad de generar un nombre de archivo aleatorio
+            string prefix = System.IO.Path.GetRandomFileName().Replace(".", "").Substring(0, 3);
+
+            Random value = new Random();
+
+            int suffix = value.Next(0000, 9999);
+
+            return prefix + suffix.ToString();
+        }
+    }
+}
